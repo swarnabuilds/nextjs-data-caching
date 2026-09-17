a2723e585be46b26353254de9bc1a13506f87c65
swarna38 <swarna.sutradhar.info@gamil.com>
2026-09-17 22:56:49 +0600

    3 way data fetching

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const PostPage = () => {
+//data fetch option-1
+// const postPromise = async() => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+//     if(!res.ok){
+//         throw new Error('Failed to fetch posts')
+//     }
+//     return res.json()
+// }
+
+
+//data fetch option-2
+const postPromise = async() => {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+        return res.json()
+    }
+    catch(error) {
+        throw new Error('Failed to fetch posts')
+    }
+}
+
+const PostPage = async() => {
+    const posts = await postPromise()
     return (
         <div>
-            <h2>al posts page</h2>
+            <h2>al posts page {posts.length}</h2>
         </div>
     );
 };
